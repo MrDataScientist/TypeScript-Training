b98e854cb28276f99330f67f0bc7091300f79e04
Tarik EN NAKDI <t.ennakdi@gmail.com>
2019-01-16 15:27:20 +0100

    playing with type anotation

--- samples/hello-world.js
+++ samples/hello-world.js
@@ -1,2 +1,8 @@
 var message = 'hello world';
+if (message) {
+    var i;
+    for (i = 0; i < 3; i++) {
+        console.log(message + ' ' + i);
+    }
+}
 console.log(message);
--- samples/hello-world.ts
+++ samples/hello-world.ts
@@ -1,3 +1,10 @@
 let message = 'hello world';
 
+if (message){
+    var i:number;
+
+    for(i=0;i<3;i++){
+        console.log(message + ' '+i);
+    }
+}
 console.log(message);
