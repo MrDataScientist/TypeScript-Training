8585e1d65bf4eb7c35ad2f2e3ef503ea7a16e66c
Tarik EN NAKDI <t.ennakdi@gmail.com>
2019-01-19 14:40:01 +0100

    updates

---
+++ samples/learning-types.js
@@ -0,0 +1,5 @@
+//const player:anykugli = {firstName:"magic",lastName:"johnson",address:"staples"};
+var _ = require('lodash');
+var colors = ["red", "blue", "green"];
+var firstColor = _.first(colors);
+console.log(firstColor);
---
+++ samples/learning-types.ts
@@ -0,0 +1,8 @@
+//const player:anykugli = {firstName:"magic",lastName:"johnson",address:"staples"};
+
+const _= require('lodash');
+
+const colors = ["red","blue","green"];
+
+const firstColor= _.first(colors);
+console.log(firstColor);
