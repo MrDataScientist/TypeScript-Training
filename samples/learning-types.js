3cb6ecd51cc89f04989f08cc81acc4ad91c0b794
Tarik EN NAKDI <t.ennakdi@gmail.com>
2019-01-19 14:42:39 +0100

    Adding lodash

--- samples/learning-types.js
+++ samples/learning-types.js
@@ -1,5 +1,9 @@
+"use strict";
 //const player:anykugli = {firstName:"magic",lastName:"johnson",address:"staples"};
-var _ = require('lodash');
+Object.defineProperty(exports, "__esModule", { value: true });
+//const _= require('lodash');
+var _ = require("lodash");
 var colors = ["red", "blue", "green"];
 var firstColor = _.first(colors);
 console.log(firstColor);
+//# sourceMappingURL=learning-types.js.map
--- samples/learning-types.ts
+++ samples/learning-types.ts
@@ -1,8 +1,11 @@
 //const player:anykugli = {firstName:"magic",lastName:"johnson",address:"staples"};
 
-const _= require('lodash');
+//const _= require('lodash');
+
+import * as _ from 'lodash';
 
 const colors = ["red","blue","green"];
 
 const firstColor= _.first(colors);
+
 console.log(firstColor);
