03be6cda145fb70dc2b4da7daffb685684ad5a03
Tarik EN NAKDI <t.ennakdi@gmail.com>
2019-01-29 14:00:18 +0100

    dbUrl for postgres

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,5 @@
 import * as ORM from 'sequelize'
-
+const dbUrl = 'postgres://postgres:postgres@localhost:5432/complete-typescript-course'
 const sequelize = new ORM()
 
 console.log('server is running...' )
