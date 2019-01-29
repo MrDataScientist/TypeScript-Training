ce71cd4298fd9302eb0fc26a371b2818852c022a
Tarik EN NAKDI <t.ennakdi@gmail.com>
2019-01-29 14:06:22 +0100

    updates

--- server/server.js
+++ server/server.js
@@ -1,3 +1,7 @@
-var ORM = require('sequelize');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var ORM = require("Sequelize");
+var dbUrl = 'postgres://postgres:postgres@localhost:5432/complete-typescript-course';
+var sequelize = new ORM(dbUrl);
 console.log('server is running...');
 //# sourceMappingURL=server.js.map
--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,6 @@
-import * as ORM from 'sequelize'
+import * as ORM from 'Sequelize'
+import {Sequelize} from 'Sequelize'
 const dbUrl = 'postgres://postgres:postgres@localhost:5432/complete-typescript-course'
-const sequelize = new ORM()
+const sequelize:sequelize = new ORM(dbUrl)
 
 console.log('server is running...' )
